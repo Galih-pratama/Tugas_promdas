3c54dad0099a181904df363d07ec436df48ed94f
Galih-pratama <72766559+Galih-pratama@users.noreply.github.com>
2020-10-26 21:57:39 +0700

    Update Program tabel perkalian.cpp

--- Program tabel perkalian.cpp
+++ Program tabel perkalian.cpp
@@ -7,18 +7,19 @@ using namespace std;
 int main(){
 	
 	cout << "\t TABEL PERKALIAN 1 -10 \n\n";
-	
+	cout << "\t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9 \t10 \n";
 	 
-	for(int i = 1; i <= 10; i++)
-	{
+	for(int i = 1; i <= 10; i++){
+	        cout << i << endl;
+	
 		
 		for(int j = 1; j<= 10; j++)
-		{
-			
-			cout << " " << i * j << "\t ";		 
+		{	
+			cout << " \t ";
+			cout << " * ";
 		}
 		
-		cout << endl << endl;
+		cout << endl;
 	}
 	  
 }
